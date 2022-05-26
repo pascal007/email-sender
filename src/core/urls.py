69f8a28b7f25a80ec81aa886aca477c22bd8554c
Pascal Ezeama <pascalezeama@gmail.com>
2022-05-26 09:48:37 +0100

    saving changes

--- src/core/urls.py
+++ src/core/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='index.html'), name='home-view'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('sign-up', SignUpView.as_view(), name='sign-up'),
-    path('dashboard/', DashboardView.as_view(), name='dashboard')
+    path('', DashboardView.as_view(), name='dashboard')
 ]
 
 if settings.DEBUG:
